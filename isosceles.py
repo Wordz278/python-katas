30cc6fd30d01fab5f913fd70a3906574a0cdf9a8
Adrian Floyd <adrianshikwambana@umuzi.org>
2020-02-12 11:49:48 +0200

    updated isosceles function

--- isosceles.py
+++ isosceles.py
@@ -1,13 +1,18 @@
-def isosceles():
-    k = 0
-    num = int(input("Enter the triangle figure : "))
-    for i in range(1, num+1):
-        for j in range(1, (num-i)+1):
+def isosceles(number):
+    initial_value = 0
+    return_result = ''
+    for column in range(1, number+1):
+        for row in range(1, (number-column)+1):
             print(end="  ")
-        while k != (2*i-1):
+        while initial_value != (2*column-1):
             print("# ", end="")
-            k += 1
-        k = 0
+            initial_value += 1
+        initial_value = 0
         print()
+    return return_result
 
-isosceles()
+
+result1 = isosceles(2)
+result2 = isosceles(4)
+print(result1)
+print(result2)
