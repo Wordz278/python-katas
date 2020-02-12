f7bbc12be220a5334bebdca0863c2f8f347f4dd7
Adrian Floyd <adrianshikwambana@umuzi.org>
2020-02-12 11:48:42 +0200

    updated frame function

--- frame.py
+++ frame.py
@@ -1,9 +1,11 @@
-sentence=input("Enter your sentence : ")
-
-def frame(words):
+def frame(words=[]):
+    result_empty = ''
     size = len(max(words, key=len))
     print('*' * (size + 4))
     for word in words:
         print('* {a:<{b}} *'.format(a=word, b=size))
     print('*' * (size + 4))
-frame(sentence.split(" "))
+    return result_empty
+
+sentence = frame(["Write", "good", "code", "everyday"])
+
