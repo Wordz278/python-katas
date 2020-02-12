8b94c2612230b222f894ae14827f8b9b7038f2bd
Adrian Floyd <adrianshikwambana@umuzi.org>
2020-02-12 11:52:37 +0200

    updated triangle function

--- triangle.py
+++ triangle.py
@@ -1,8 +1,13 @@
-def triangle():
-    num = int(input("Enter the triangle measures : "))
-    for i in range(num):
-        for j in range(i + 1):
+def triangle(number):
+    return_result = ''
+    for column in range(number):
+        for row in range(column + 1):
             print('#', end = ' ')
         print()
+    return return_result
 
-triangle()
+triangle_result1 = triangle(2)
+triangle_result2 = triangle(4)
+
+print(triangle_result1)
+print(triangle_result2)
