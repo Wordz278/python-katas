ddef324387161d02848e698f18eeba5d31ef429b
Adrian Floyd <adrianshikwambana@umuzi.org>
2020-02-12 11:47:09 +0200

    updated combined list

--- combine_list.py
+++ combine_list.py
@@ -1,17 +1,10 @@
-def merge():
-    list1 = []
-    list2 = []
-    
-    lst1 = input("Enter numbers for list 1 : ").split(" ")
-    list1 = lst1
-    
-    lst2 = input("Enter numbers for list 2 : ").split(" ")
-    list2 = lst2
-
+def merge(list1=[], list2=[]):    
     n = len(list1)
     store_list = []
     for i in range(n):
         store_list.append(list1[i])
         store_list.append(list2[i])
-    print(store_list)
-merge()
+    return store_list
+
+combined_list = merge([11,22,33], [1,2,3]) 
+print(combined_list)
