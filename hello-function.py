bfdf1d40b1b50291a01714e10c065f23cda891c2
Adrian Floyd <adrianshikwambana@umuzi.org>
2020-02-12 11:49:13 +0200

    updated hello word function

--- hello-function.py
+++ hello-function.py
@@ -1,5 +1,6 @@
-def Hello(fname):
-    fname = input("Please Enter your name : ")
-    print("Hello " + fname + "!")
-
-Hello("")
+def hello_function(name):
+    hello_name = f"Hello {name}!"
+    return hello_name
+    
+my_name = hello_function("Adrian")
+print(my_name)
